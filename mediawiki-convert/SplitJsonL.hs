46d419caea6f67f216de1a75448ca6e3f2605dc8
Laura Dietz <dietz@cs.umass.edu>
2022-01-16 10:25:53 -0500

    SplitJsonL change BSL.split '\n' to BSL.lines
    this avoids getting empty newlines at the end of the last jsonl split

--- mediawiki-convert/SplitJsonL.hs
+++ mediawiki-convert/SplitJsonL.hs
@@ -10,20 +10,19 @@
 import Data.Monoid hiding (All, Any)
 import Options.Applicative
 import Control.Monad
-import qualified Data.HashSet as HS
+-- import qualified Data.HashSet as HS
 import qualified Data.Text as T
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 import Text.PrettyPrint.ANSI.Leijen ((<$$>))
-import Data.Maybe
-import Data.Char
--- import qualified Data.ByteString.Lazy as BSL
+-- import Data.Maybe
+-- import Data.Char
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import qualified Codec.Compression.GZip as GZip
-import Codec.Compression.Zlib.Internal (DecompressError)
+-- import Codec.Compression.Zlib.Internal (DecompressError)
 import Data.List.Split as S
 
-import Data.Aeson as Aeson
-import Data.Aeson.Types as Aeson
+-- import Data.Aeson as Aeson
+-- import Data.Aeson.Types as Aeson
 
 import SimplIR.DataSource.Compression.Lazy
 import CAR.ToolVersion
@@ -32,14 +31,11 @@ helpDescr :: PP.Doc
 helpDescr =
     "Split JSONL into multiple archives of `n` entries " <$$> PP.indent 4 options
   where
-    cmd a b = PP.nest 8 (a <$$> b)
+    -- cmd a b = PP.nest 8 (a <$$> b)
     options = PP.vsep
       [
         -- cmd "need doc"                           "here"
       ]
-
-data ConfOpts = ConfOpts { 
-                         }
 
 opts :: Options.Applicative.Parser (FilePath, FilePath, Int)
 opts =
@@ -52,21 +48,21 @@ opts =
 --  -------------- file writing and command line handling -------------
 
 writeGzJsonLFile ::  FilePath -> [BSL.ByteString] -> IO()
-writeGzJsonLFile fname lines = do
+writeGzJsonLFile fname lnes = do
     BSL.writeFile fname 
         $ GZip.compressWith (GZip.defaultCompressParams { GZip.compressLevel = GZip.bestSpeed })  
-        $ BSL.unlines $ lines
+        $ BSL.unlines $ lnes
     Prelude.putStrLn  $ "Writing JsonL.gz to "<> fname
 
 main :: IO ()
 main = do
     (inputFile, outputFileTemplate, numLines) <- execParser' 1 (helper <*> opts) (progDescDoc $ Just helpDescr)
-    lines <- BSL.split '\n' . decompress <$> BSL.readFile inputFile
+    lns <- BSL.lines . decompress <$> BSL.readFile inputFile
           :: IO [BSL.ByteString] 
-    let chunks = S.chunksOf numLines lines
-    forM_ (zip [1..] chunks) (\(i, chunk) -> 
+    let chunks = S.chunksOf numLines lns
+    forM_ (zip [1::Integer ..] chunks) (\(i, chnk) -> 
             let outputFile = filePattern outputFileTemplate i
-            in writeGzJsonLFile outputFile chunk
+            in writeGzJsonLFile outputFile chnk
         )
   where filePattern outputFileTemplate i =
             T.unpack
